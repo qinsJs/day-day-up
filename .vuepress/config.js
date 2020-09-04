2098214b24a24843072075b0bc225b365fed4863
qins <694666422@qq.com>
2020-09-04 22:12:15 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: '天天向上',
+  description: '好好学习，天天向上',
+   head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
+  port:12345,
+  themeConfig: {
+    nav: [
+      { text: '2020', link: '/2020/' },
+      { 
+	    text: '连接', 
+		items: [
+          { text: '现在还没有', link: '/' },
+          { text: '但是会有的', link: '/' }
+        ]
+	  },
+    ],
+	
+    sidebar:{
+      '/2020/':[
+        {
+          title: '9月',
+          children: [ 
+            '/2020/9/4',
+            '/2020/9/5'
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /logo.svg
+heroText: day day up
+tagline: 好好学习，天天向上
+actionText: 开始复习 →
+actionLink: /2020/
+features:
+- title: 不忘初心
+  details: 生活虽苦，却不能忘了自己的初心。
+- title: 水水提交
+  details:  git commit 不在是白板 ~
+footer: MIT Licensed | Copyright © 2018-present 
+---
